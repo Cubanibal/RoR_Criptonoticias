c019c046f6658f2d3d8475ea5c66fb6234302a6b
Anibal <cubanibal@hotmail.com>
2021-11-18 14:07:37 +0100

    instalado bootstrap y stimullus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import * as bootstrap from 'bootstrap'
+import "../stylesheets/application"
+import 'controllers'
 
 Rails.start()
 Turbolinks.start()
