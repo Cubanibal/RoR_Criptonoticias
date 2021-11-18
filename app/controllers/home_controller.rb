4c67c60c7599242c75f28517cc60a97351e560a0
Anibal <cubanibal@hotmail.com>
2021-11-18 16:51:27 +0100

    Conectado api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  
   def index
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&CMC_PRO_API_KEY=c93b03ea-b0d3-4f24-8908-2c2ac8494af0'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @array1 = JSON.parse(@response).to_a
+    @array2 =@array1[1].to_a
+    
+    @coins = @array2[1].to_a
   end
+
+  def precios
+  end
+  
 end
